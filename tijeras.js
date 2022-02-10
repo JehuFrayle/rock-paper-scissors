649646d6128a888c139b04f552dd8c0eb03df231
Jehu Frayle <jehufrayle@gmail.com>
2022-02-10 11:42:09 -0800

    I added a first interface: game buttons, reset button and score board. Decent CSS.

--- tijeras.js
+++ tijeras.js
@@ -1,62 +1,103 @@
 var usuario;
 var maquina;
 
-var papel = "Papel";
-var piedra = "Piedra";
-var tijeras = "Tijeras";
+var rock = "piedra";
+var paper = "papel";
+var scissors = "tijeras";
+
+var scoreBoard = document.getElementById("score");
+
+function makeCounter(){
+    let counter = 0;
+
+    return {
+        increase: function(){
+            counter ++;
+            return counter;
+        },
+        decrease: function(){
+            counter = counter -1;
+            return counter;
+        },
+        getValue: function(){
+            return counter;
+        },
+        toZero: function(){
+            counter = 0;
+            return counter;
+        }
+    }
+}
+
+const cpuScore = makeCounter();
+const usrScore = makeCounter();
+
 
 function quienGana(usr, cpu) {
-    if (usr == piedra) {
-        if (cpu == piedra) {
+    if (usr == "piedra") {
+        if (cpu == "piedra") {
             alert('Empate!');
         }
-        else if (cpu == papel) {
-            alert('Perdiste!')
-        }
-        else if (cpu = tijeras) {
-            alert('Ganaste!')
-        }
-    }
-    if (usr == papel) {
-        if (cpu == piedra) {
-            alert('Ganaste!');
-        }
-        else if (cpu == papel) {
-            alert('Empate!')
-        }
-        else if (cpu = tijeras) {
-            alert('Perdiste!')
-        }
-    }
-    if (usr == tijeras) {
-        if (cpu == piedra) {
+        else if (cpu == "papel") {
             alert('Perdiste!');
+            cpuScore.increase();
         }
-        else if (cpu == papel) {
+        else if (cpu == "tijeras") {
+            alert('Ganaste!');
+            usrScore.increase();
+        }
+    }
+    if (usr == "papel") {
+        if (cpu == "piedra") {
+            alert('Ganaste!');
+            usrScore.increase();
+        }
+        else if (cpu == "papel") {
+            alert('Empate!')
+        }
+        else if (cpu == "tijeras") {
+            alert('Perdiste!')
+            cpuScore.increase();
+        }
+    }
+    if (usr == "tijeras") {
+        if (cpu == "piedra") {
+            alert('Perdiste!');
+            cpuScore.increase();
+        }
+        else if (cpu == "papel") {
             alert('Ganaste!')
+            usrScore.increase();
         }
-        else if (cpu = tijeras) {
+        else if (cpu == "tijeras") {
             alert('Empate!')
         }
     }
+    console.log(`CPU: ${cpuScore.getValue()}, USER: ${usrScore.getValue()}`)
+    scoreBoard.innerHTML = `[${usrScore.getValue()} : ${cpuScore.getValue()}]`;
+
 }
 
 function eleccionCPU() {
     var numeroAleatorio = Math.floor((Math.random() * (4-1)) +1); // genera numero entre el 1 y el 3
     if(numeroAleatorio == 1) {
-        maquina = piedra;
+        maquina = "piedra";
     }
     else if (numeroAleatorio == 2) {
-        maquina = papel;
+        maquina = "papel";
     }
     else if (numeroAleatorio == 3) {
-        maquina = tijeras;
+        maquina = "tijeras";
     }
 
     console.log(numeroAleatorio);
-    console.log ("La maquina escogio: " + maquina);
+    console.log ("La maquina ha escogido: " + maquina);
 
     return maquina;
 }
 
-quienGana(piedra, eleccionCPU());
+function reset() {
+    usrScore.toZero();
+    cpuScore.toZero();
+    scoreBoard.innerHTML = `[${usrScore.getValue()} : ${cpuScore.getValue()}]`;
+}
